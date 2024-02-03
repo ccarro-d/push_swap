3ee672569b1f80e1e31ac34574445b58fee6040a
Cesar Carro diaz <ccarro-d@c3r7s6.42madrid.com>
2024-02-03 19:49:14 +0100

    03/02/2024 - 42

--- ft_parser.c
+++ ft_parser.c
@@ -6,7 +6,7 @@
 /*   By: ccarro-d <ccarro-d@student.42madrid.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/01/23 19:34:49 by ccarro-d          #+#    #+#             */
-/*   Updated: 2024/01/24 21:15:26 by ccarro-d         ###   ########.fr       */
+/*   Updated: 2024/02/03 19:41:21 by ccarro-d         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,12 +15,12 @@
 void	ft_parser(char **args, t_list **stack, int size)
 {
 	int	i;
-	t_list	init_stack;
+	t_list	*stack_member;
 
 	i = 0;
-	while (args != '\0')
+	while (size > 0)
 	{
-		
+		stack_member = ft_lstnew((void *)args[size--]);
+		ft_lstadd_back(stack, stack_member);
 	}
-	
 }
--- main.c
+++ main.c
@@ -6,24 +6,34 @@
 /*   By: ccarro-d <ccarro-d@student.42madrid.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/01/23 19:08:20 by ccarro-d          #+#    #+#             */
-/*   Updated: 2024/01/24 21:57:08 by ccarro-d         ###   ########.fr       */
+/*   Updated: 2024/02/03 19:46:47 by ccarro-d         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "push_swap.h"
 
+void	print_list(t_list *lst)
+{
+	t_list	*aux;
+	size_t	lst_size;
+
+	aux = lst;
+	lst_size = ft_lstsize(lst);
+	while (lst_size > 0)
+	{
+		printf("%s\n", (char *)lst->content);
+		lst_size--;
+	}
+}
+
 int	main (int argc, char **argv)
 {
-	int i;
 	t_list	*stack_a;
 	t_list	*stack_b;
 	
-	i = argc - 1;
 	(void)stack_b;
-	(void)stack_a;
 	if (argc <= 1)
 		return (0);
-	ft_parser(argv, stack_a, i);
-	/*new = ft_lstnew(argv[i]);
-	ft_lstadd_back(stack_a, new);*/
+	ft_parser(argv, &stack_a, argc - 1);
+	print_list(stack_a);
 }
